d3d15d77723afe324bc43a0daaf4f501f3de3310
Dat <dat.lemindast@gmail.com>
2024-06-03 14:36:07 +0700

    add pytest in git workflow

--- test/test.py
+++ test/test.py
@@ -1,34 +1,30 @@
-import pandas as pd
-import pyodbc
-from .controls.control import *
+import pylint
+import pytest
+from src import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
-    # Read Excel data into a DataFrame
-    column_names = ['ID', 'DateTime', 'Line', 'SKUID', 'ProductName', 'Barcode', 'Status', 'Reject']
+    @pytest.fixture
+    def client():
+        with app.test_client() as client:
+            with app.app_context():
+                yield client
 
-    # Read Excel data into a DataFrame with specified column names
-    excel_file = 'data.xlsx'
-    df = pd.read_excel(excel_file, names=column_names)
-    connection = pyodbc.connect(driver="ODBC Driver 17 for SQL Server",
-        server='localhost',
-        database='Vision_Mas140',
-        uid='sa',  # Replace with your username
-        pwd='Password.1',  # Replace with your password
-        port=1433)   
-    cursor = connection.cursor()
-    for index, row in df.iterrows():
-        # Construct the SQL INSERT statement
-        sql = "INSERT INTO Table_ResultCarton (ID, DateTime, Line, SKUID, ProductName, Barcode, Status, Reject) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
-        values = (str(row['ID']), row['DateTime'], row['Line'], str(row['SKUID']),
-              row['ProductName'], str(row['Barcode']), row['Status'], row['Reject'])
-        print(values)
+    def test_home(client):
+        response = client.get('/')
+        json_data = response.get_json()
+        assert response.status_code == 200
+        assert json_data['message'] == 1
 
-        # Execute the SQL statement
-        cursor.execute(sql, values)
+    def test_add(client):
+        response = client.get('/login')
+        json_data = response.get_json()
+        assert response.status_code == 200
+        assert json_data['result'] == 1
 
-    # Commit the transaction (save changes to the database)
-    connection.commit()
+        response = client.get('/home')
+        json_data = response.get_json()
+        assert response.status_code == 200
+        assert json_data['result'] == 1
 
-    # Close the cursor and connection
-    cursor.close()
-    connection.close()
