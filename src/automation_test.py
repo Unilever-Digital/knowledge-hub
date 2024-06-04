08507af0fd2ad72c0a2afd5f45d29441194f710b
Dat <dat.lemindast@gmail.com>
2024-06-04 19:13:36 +0700

    update git test workflow

--- src/automation_test.py
+++ src/automation_test.py
@@ -9,17 +9,31 @@ def client():
         with app.app_context():
             yield client
 
+
 # Index link test connection
 def test_index(client):
     response = client.get('/')
     html_data = response.data.decode('utf-8')
     print(html_data)
 
+
 # Login page test connection
 def test_login(client):
     response = client.get('/login')
     html_data = response.data.decode('utf-8')
     print(html_data)
+
+
+# Login action test
+def test_login_post(client):
+    response = client.post('/login', data=dict(
+        username='unilever',
+        password='testpassword'
+    ), follow_redirects=True)
+    assert response.status_code == 200
+    html_data = response.data.decode('utf-8')
+    print(html_data)
+
 
 # Home page test connection
 def test_home(client):
@@ -29,11 +43,11 @@ def test_home(client):
 
 if __name__ == "__main__":
     """Configuration of automation test
-
     Yields:
         _func: _router test_
     """
-    client = client()
-    test_index(client)
-    test_login(client)
-    test_home(client)
+    for index in range(100):
+        client = client()
+        test_index(client)
+        test_login(client)
+        test_home(client)
