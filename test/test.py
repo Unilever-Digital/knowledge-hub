baf2574c7c5bdebcfddb21086207e82845d85eef
Dat <dat.lemindast@gmail.com>
2024-06-04 11:18:48 +0700

    update automation test git wl

--- test/test.py
+++ test/test.py
@@ -2,6 +2,33 @@ import pytest
 from src import create_app
 
 app = create_app()
+@pytest.fixture
+# client component app maker 
+def client():
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
+
+# index link test connection
+def test_index(client):
+    response = client.get('/')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['message'] == 1
+
+# login page test connection
+def test_login(client):
+    response = client.get('/login')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['result'] == 1
+        
+        
+def test_home(client):
+    response = client.get('/home')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['result'] == 1
 
 if __name__ == "__main__":
     """configuration of automation test
@@ -9,26 +36,8 @@ if __name__ == "__main__":
     Yields:
         _func: _router test_
     """
-    @pytest.fixture
-    def client():
-        with app.test_client() as client:
-            with app.app_context():
-                yield client
-
-    def test_home(client):
-        response = client.get('/')
-        json_data = response.get_json()
-        assert response.status_code == 200
-        assert json_data['message'] == 1
-
-    def test_add(client):
-        response = client.get('/login')
-        json_data = response.get_json()
-        assert response.status_code == 200
-        assert json_data['result'] == 1
-
-        response = client.get('/home')
-        json_data = response.get_json()
-        assert response.status_code == 200
-        assert json_data['result'] == 1
-
+    test_index(client)
+    test_login(client)
+    test_home(client)
+    
+   
